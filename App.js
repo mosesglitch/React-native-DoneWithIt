894964c25dd261cb1b5ca6ae52041cd42e328e90
mosesglitch <mosesgachuru15@gmail.com>
2023-01-31 19:39:35 +0300

    state in react-native

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text,TouchableWithoutFeedback, View,Image, SafeAreaView } from 'react-native';
+import {React, useState,useEffect} from 'react';
+import { StyleSheet, Text,Button,TouchableHighlight, View,Image,Alert, SafeAreaView } from 'react-native';
+// import ButtonResponse from './components/ButtonResponse.js';
+
+const names=['green','red','pink','blue']
 
 export default function App() {
-  let x=1
+  const [color,setColor]=useState('green')
+  const [showMessage,setShowMessage]=useState(false)
+
+  const b=4
+  const c=7
+  const d=b+c
+
   const handlePress=()=>{
     console.log("pressed ooooo")
   }
   console.log("app executed")
+  const myNames=names.map((name)=>{return <Text onPress={(e)=>setColor(e._dispatchInstances.memoizedProps.children)}>{name}</Text>})
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={5} onPress={()=>handlePress()}>Hallo world, How are you.Good A really really long text, make this longer and see what happens</Text>
       <Image source={require("./assets/favicon.png")}/>
-      <TouchableWithoutFeedback onPress={()=>console.log("image tapped")}>
+      <TouchableHighlight onPress={()=>console.log("image tapped")}>
       <Image 
       blurRadius={5}
       fadeDuration={1000}
@@ -20,8 +31,20 @@ export default function App() {
         height:300,
         uri:"https://picsum.photos/200/300"}}/>
 
-      </TouchableWithoutFeedback>
+      </TouchableHighlight>
+      <View style={{width:200,height:70,backgroundColor:`${color}`}}>
+        <Text>{d}</Text>
+      </View>
+      <Button color="green" style={{color:"green"}} title="Click" onPress={()=>alert("button tapped")}/>
+      {showMessage ? <Text>Howdy</Text> : <Text>Wimwega</Text>}
+      <Button color="green" style={{color:"green"}} title="Texts show" onPress={()=>setShowMessage(true)}/>
+
+      {/* <Button color="green" style={{color:"green"}} title="Click" onPress={()=>Alert.alert("mytitle","My message",
+      [{text}])}/> */}
+       {myNames}
+       <Text>{color}</Text>
       <StatusBar style="auto" />
+      {/* <ButtonResponse/> */}
     </SafeAreaView>
   );
 }
@@ -29,7 +52,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/ButtonResponse.js
@@ -0,0 +1,9 @@
+import {  View,Text } from 'react-native';
+
+const ButtonResponse=()=>{
+    const b=4
+    const c=7
+    const d=b+c
+return <Text>{d}</Text>
+}
+export default ButtonResponse;
